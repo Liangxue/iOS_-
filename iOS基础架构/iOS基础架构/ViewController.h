4a30c6bd73ee7845398c84a38845b2f42c2981b8
liangxue <15145078036@163.com>
2017-04-25 10:35:32 +0800

    iOS基础架构
    iOS基础架构

---
+++ iOS基础架构/iOS基础架构/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS基础架构
+//
+//  Created by xue on 2017/4/25.
+//  Copyright © 2017年 liangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS基础架构/iOS基础架构/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS基础架构
+//
+//  Created by xue on 2017/4/25.
+//  Copyright © 2017年 liangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
